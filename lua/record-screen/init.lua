fb887412faeb3599f4c3c53cd726d20d58fe9686
Eric Wong <wsdjeg@outlook.com>
2025-04-07 23:55:20 +0800

    update

--- lua/record-screen/init.lua
+++ lua/record-screen/init.lua
@@ -1,23 +1,21 @@
 local M = {}
 
-local target_dir = "~/Desktop/"
-local job = require('job')
-local nt = require('notify')
+local target_dir = "C:/User/wsdjeg/Desktop/"
+local job = require("job")
+local nt = require("notify")
 local jobid
 
 function M.start(opt)
-	local cmd =
-		{ "ffmpeg", "-f", "gdigrab", "-r", "60", "-i", "desktop", "-s", "1920x1080", target_dir .. "output.mp4" }
-    jobid = job.start(cmd, {
-        on_exit = function(id, data, single)
-            nt.notify(string.format('job exit with: code %s single %s', data, single))
-        end
-    })
+	local cmd = { "ffmpeg", "-f", "gdigrab", "-i", "desktop", "-f", "mp4", target_dir .. "output.mp4" }
+	jobid = job.start(cmd, {
+		on_exit = function(id, data, single)
+			nt.notify(string.format("job exit with: code %s single %s", data, single))
+		end,
+	})
 end
 
 function M.stop()
-
-    job.send(jobid, 'q')
+	job.send(jobid, "q")
 end
 
 function M.setup(opt) end
